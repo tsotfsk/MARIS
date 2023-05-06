337bc9678c574db789a9bbf1132458f7da3f88b0
tsotfsk <tsotfsk@qq.com>
2023-05-06 18:39:05 +0800

    FEA: update codes

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,4 @@
 from models.GRU4Rec.gru4rec import GRU4Rec
 from models.GRU4RecF.gru4recf import GRU4RecF
 from models.MARIS.maris import MARIS
-from models.MARIS.sup import SUP
-# from models.MARIS.agent import ModalAgent
-# from models.MARIS.fusion import Fusion
 from models import layers
